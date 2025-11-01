f3ba0c9550233ff1c5a825b2204f330fa9420ae4
Prayag Singh <prayags@amazon.com>
2025-11-01 11:17:29 -0700

    deployment spec modification

--- internal/controller/deployment_builder_access.go
+++ internal/controller/deployment_builder_access.go
@@ -9,6 +9,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // partialAccessResourceData provides values for template substitutions
@@ -122,11 +123,9 @@ func (db *DeploymentBuilder) ApplyAccessStrategyToDeployment(
 		return fmt.Errorf("failed to add environment variables to container: %w", err)
 	}
 
-	// Conditional sidecar logic based on access strategy name
-	if accessStrategy.Name == "aws-ssm-remote-access" {
-		if err := db.addSSMSidecarContainer(deployment, accessStrategy); err != nil {
-			return fmt.Errorf("failed to add SSM sidecar container: %w", err)
-		}
+	// Apply deployment spec modifications if defined
+	if err := db.applyDeploymentSpecModifications(deployment, accessStrategy); err != nil {
+		return fmt.Errorf("failed to apply deployment spec modifications: %w", err)
 	}
 
 	return nil
@@ -204,5 +203,79 @@ func (db *DeploymentBuilder) addSSMSidecarContainer(
 		ssmContainer,
 	)
 
+	return nil
+}
+
+// applyDeploymentSpecModifications applies deployment modifications from access strategy
+func (db *DeploymentBuilder) applyDeploymentSpecModifications(
+	deployment *appsv1.Deployment,
+	accessStrategy *workspacev1alpha1.WorkspaceAccessStrategy,
+) error {
+	if accessStrategy.Spec.DeploymentSpecModifications == nil {
+		return nil // Nothing to do
+	}
+
+	mods := accessStrategy.Spec.DeploymentSpecModifications
+	
+	// Add volumes
+	if mods.PodSpec != nil && len(mods.PodSpec.Volumes) > 0 {
+		logf.Log.V(1).Info("Adding volumes from deployment spec modifications", 
+			"accessStrategy", accessStrategy.Name, 
+			"volumeCount", len(mods.PodSpec.Volumes))
+		
+		deployment.Spec.Template.Spec.Volumes = append(
+			deployment.Spec.Template.Spec.Volumes, 
+			mods.PodSpec.Volumes...,
+		)
+		
+		for _, volume := range mods.PodSpec.Volumes {
+			logf.Log.V(1).Info("Added volume", 
+				"accessStrategy", accessStrategy.Name,
+				"volumeName", volume.Name)
+		}
+	}
+	
+	// Add volume mounts to primary container
+	if mods.PrimaryContainer != nil && len(mods.PrimaryContainer.VolumeMounts) > 0 {
+		logf.Log.V(1).Info("Adding volume mounts to primary container", 
+			"accessStrategy", accessStrategy.Name, 
+			"mountCount", len(mods.PrimaryContainer.VolumeMounts))
+		
+		primaryContainer := &deployment.Spec.Template.Spec.Containers[0]
+		primaryContainer.VolumeMounts = append(
+			primaryContainer.VolumeMounts, 
+			mods.PrimaryContainer.VolumeMounts...,
+		)
+		
+		for _, mount := range mods.PrimaryContainer.VolumeMounts {
+			logf.Log.V(1).Info("Added volume mount to primary container", 
+				"accessStrategy", accessStrategy.Name,
+				"volumeName", mount.Name,
+				"mountPath", mount.MountPath)
+		}
+	}
+	
+	// Add additional containers
+	if mods.PodSpec != nil && len(mods.PodSpec.AdditionalContainers) > 0 {
+		logf.Log.V(1).Info("Adding additional containers", 
+			"accessStrategy", accessStrategy.Name, 
+			"containerCount", len(mods.PodSpec.AdditionalContainers))
+		
+		deployment.Spec.Template.Spec.Containers = append(
+			deployment.Spec.Template.Spec.Containers,
+			mods.PodSpec.AdditionalContainers...,
+		)
+		
+		for _, container := range mods.PodSpec.AdditionalContainers {
+			logf.Log.V(1).Info("Added additional container", 
+				"accessStrategy", accessStrategy.Name,
+				"containerName", container.Name,
+				"image", container.Image)
+		}
+	}
+	
+	logf.Log.Info("Successfully applied deployment spec modifications", 
+		"accessStrategy", accessStrategy.Name)
+	
 	return nil
 }
